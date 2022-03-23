edc04c223faf016d547a3fc0d1be69269da4fb4d
Mullajanov <seanmullajanov@gmail.com>
2022-03-22 20:39:23 -0700

    Verify Checked By Default And Uncheck One Option, actions to navigate plus navigate to filter button has been created

--- src/test/java/com/vytrack/tests/US_14_VerifyCheckedAndUnchecked.java
+++ src/test/java/com/vytrack/tests/US_14_VerifyCheckedAndUnchecked.java
@@ -1,4 +1,28 @@
 package com.vytrack.tests;
-public class US_14_VerifyCheckedAndUnchecked {
 
+import com.vytrack.utilities.BrowserUtils;
+import com.vytrack.utilities.Driver;
+import com.vytrack.utilities.VytrackUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+public class US_14_VerifyCheckedAndUnchecked {
+    @Test
+    public void manageFilters_SalesManager() {
+       VytrackUtils.loginAsSalesManager();
+
+
+        WebElement marketingBtn = Driver.getDriver().findElement(By.xpath("(//span[@class='title title-level-1'])[6]"));
+        //Create actions to navigate and click the Campaigns under the Marketing
+        Actions actions = new Actions(Driver.getDriver());
+        actions.moveToElement(marketingBtn).perform();
+        BrowserUtils.sleep(2);
+        WebElement campaignsBtn = Driver.getDriver().findElement(By.xpath("//*[@id=\"main-menu\"]/ul/li[6]/div/div/ul/li[4]/a/span"));
+        campaignsBtn.click();
+        //navigate on filter button and click
+        WebElement filterBtn = Driver.getDriver().findElement(By.xpath("//*[@title = 'Filters']"));
+        filterBtn.click();
+    }
 }
