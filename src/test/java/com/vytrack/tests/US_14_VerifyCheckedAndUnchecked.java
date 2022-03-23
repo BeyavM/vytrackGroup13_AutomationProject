73dca70fba7ae08c3a7642ad0a5a8c3bf1d066dd
Mullajanov <seanmullajanov@gmail.com>
2022-03-22 20:40:57 -0700

    Verify Checked By Default And Uncheck One Option , navigate on a manage filter button and click plus list of web elements to verify 6 options are checked by default is created

--- src/test/java/com/vytrack/tests/US_14_VerifyCheckedAndUnchecked.java
+++ src/test/java/com/vytrack/tests/US_14_VerifyCheckedAndUnchecked.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class US_14_VerifyCheckedAndUnchecked {
     @Test
     public void manageFilters_SalesManager() {
@@ -24,5 +26,13 @@ public class US_14_VerifyCheckedAndUnchecked {
         //navigate on filter button and click
         WebElement filterBtn = Driver.getDriver().findElement(By.xpath("//*[@title = 'Filters']"));
         filterBtn.click();
+        //navigate on a manage filter button and click
+        WebElement manageFilterBtn = Driver.getDriver().findElement(By.xpath("//*[@class= 'add-filter-button']/span"));
+        manageFilterBtn.click();
+        //Verify 6 options are checked by default
+        List<WebElement> allOptions = Driver.getDriver().findElements(By.xpath("//div[@class='ui-multiselect-menu ui-corner-all select-filter-widget dropdown-menu']/ul/li/label/input"));
+        for (WebElement option : allOptions) {
+            System.out.println("option.isSelected() = " + option.isSelected());
+        }
     }
 }
